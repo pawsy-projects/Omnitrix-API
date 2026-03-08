3faeadb53b412d533264b00392e0ad1985f4a748
Vi <144982981+rapositosa@users.noreply.github.com>
2026-03-08 10:30:48 -0400

    index.js
    pequenas mudanças

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const app = require('express')();
-const cors = require('cors');
-const compression = require('compression');
+const cors = require('cors'), compression = require('compression');
 
 app.use(cors(), compression(), require('express').json());
-app.use((req, res, next) => {
-res.setHeader('X-Content-Type-Options', 'nosniff');
-res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
-res.removeHeader('X-Powered-By');
-res.setTimeout(10000, () => { if (!res.headersSent)
-res.status(408).json({ status: 408, msg: "😔 A rota demorou demais para responder. Tente novamente." }); });
- next(); });
-
- // ~~~ 🐈 ~~~ 
-
-app.use('/api', require('./src/rotas'));
-
-app.get('/', (req, res) => res.json({ status: 200, msg: "Um projeto para exibir informações sobre o universo de Ben 10! Criado por pawsy.", url: "https://pawsy.fun" }));
-
-app.use((req, res) => {
-  res.status(404).json({ status: 404, msg: "🤔 Essa rota não foi encontrada. Verifique se escreveu corretamente." });
+app.use((req, res, next) => { 
+res.setHeader('X-Content-Type-Options', 'nosniff'); res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+  res.removeHeader('X-Powered-By');
+  next();
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ status: 500, msg: "☹️ Houve um erro interno na API.", erro: err.message });
-});
+// ~~~ 🐈 ~~~
+
+app.get("/", (req, res) => res.json({ status: 200, mensagem: "🦠 API REST pública com dados detalhados sobre os alienígenas e o universo de Ben 10. Criada por pawsy." }));
+
+try { require("./src/rotas")(app); } 
+catch (e) { console.error("> Erro:", e); }
+
+app.use((req, res) => res.status(404).json({ status: 404, erro: "😥 Essa página não foi encontrada. Certifique se escreveu corretamente." }));
 
 module.exports = app;
